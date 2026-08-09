3b2e8db3bbed2287b6a25ec15963149d35b8d26a
paulolimauai <paulolp0101@gmail.com>
2026-08-09 05:16:58 -0300

    fix: adiciona escapeHTML no escopo do servidor Node.js para envio de email de redefinicao

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ function verifyPassword(password, storedPassword) {
   return hash === originalHash;
 }
 
+function escapeHTML(str) {
+  if (str === null || str === undefined) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
 const activeSessions = new Map();
 
 function createSessionToken(user) {
